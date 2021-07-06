4939c86037f7929c1134fa6c8aff51189f07ea94
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-07-06 10:50:21 -0500

    updated on 6-7-21 10:50

---
+++ PatronObserver.js
@@ -0,0 +1,51 @@
+class Observable{
+    constructor(){
+        this.observers=[];
+    }
+
+    subscribe(notifyingClass){
+        this.observers.push(notifyingClass);
+    }
+
+    unsubscribe(notifyingClass){
+        this.observers = this.observers.filter(observer => observer instanceof notifyingClass !== true);
+    }
+
+    notifyObservable(notification){
+        this.observers.forEach(observer => {
+            observer.notify(notification);
+        });
+    }
+}
+
+class NumberExample extends Observable{
+    constructor(){
+        super();
+        this.value = 0;
+    }
+
+    increment(){
+        this.value++;
+        this.notifyObservable(this);
+    }
+}
+
+class NumberExampleSpanish{
+    notify(notification){
+        console.log(`El nuevo número es: ${notification.value}`);
+    }
+}
+
+class NumberExampleEnglish{
+    notify(notification){
+        console.log(`The new number is: ${notification.value}`);
+    }
+}
+
+let numberExample = new NumberExample();
+
+numberExample.subscribe(new NumberExampleSpanish());
+numberExample.subscribe(new NumberExampleEnglish());
+
+numberExample.increment();
+numberExample.increment();
