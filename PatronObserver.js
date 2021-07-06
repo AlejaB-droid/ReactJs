9554d99085433fd4f436ae8df90d5e98ccdd54a5
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-07-06 12:17:02 -0500

    updated on 6-7-21 12:20

--- PatronObserver.js
+++ PatronObserver.js
@@ -1,16 +1,21 @@
+//creación de clase observable
 class Observable{
     constructor(){
+        //en este arreglo se guardan los observadores
         this.observers=[];
     }
 
+    //método para suscribir observadores
     subscribe(notifyingClass){
         this.observers.push(notifyingClass);
     }
 
+    //método para desvincular observadores
     unsubscribe(notifyingClass){
         this.observers = this.observers.filter(observer => observer instanceof notifyingClass !== true);
     }
 
+    //método para notificar a cada observador sobre cambios en el objeto al que están suscritos
     notifyObservable(notification){
         this.observers.forEach(observer => {
             observer.notify(notification);
@@ -18,34 +23,43 @@ class Observable{
     }
 }
 
+//clase "hijo" de observable, es el objeto observable
 class NumberExample extends Observable{
     constructor(){
         super();
+        //se inicializa la variable (número)
         this.value = 0;
     }
 
+    //método para incrementar el objeto (número)
+    //pasa el cambio del objeto a la clase padre
     increment(){
         this.value++;
         this.notifyObservable(this);
     }
 }
 
+//subscriptor 1
 class NumberExampleSpanish{
     notify(notification){
         console.log(`El nuevo número es: ${notification.value}`);
     }
 }
 
+//susbcriptor 2
 class NumberExampleEnglish{
     notify(notification){
         console.log(`The new number is: ${notification.value}`);
     }
 }
 
+//creación de objeto observable
 let numberExample = new NumberExample();
 
+//subcriptión de los observables al observador
 numberExample.subscribe(new NumberExampleSpanish());
 numberExample.subscribe(new NumberExampleEnglish());
 
+//cambio del objeto
 numberExample.increment();
 numberExample.increment();
