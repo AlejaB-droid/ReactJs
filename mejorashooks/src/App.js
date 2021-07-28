928ee5edeeb917cd7ff878e909042a8f5afb9884
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-07-28 16:19:37 -0500

    updated on 28-7-21 16:15

--- mejorashooks/src/App.js
+++ mejorashooks/src/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-const tahoe_peaks = [
-    { name: "Freel Peak", elevation: 10891 },
-    { name: "Monument Peak", elevation: 10067 },
-    { name: "Pyramid Peak", elevation: 9983 },
-    { name: "Mt. Tallac", elevation: 9735 }
-];
-function List ({data = [], renderItem, renderEmpty}){
-    return !data.length ? (
-        renderEmpty()
-    ):(
-        <ul>
-            {data.map((item, i)=>(
-                <li key={i}>{renderItem(item)}</li>
-            ))}
-        </ul>
-    );
-}
-const RenderItem = (item)=>{
-    return(
-        <>
-            {item.name} - {item.elevation.toLocaleString()}
-        </>
-    )
-}
-const RenderEmpty =()=>{
-    return(
-        <p>Esta lista esta vacia</p>
-    );
-}
-export default function App(){
-    return(
-        <List data={ [] } renderItem={RenderItem} renderEmpty={RenderEmpty}/>
-    );
+import Home from "./components/Home";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function APP() {
+  return (
+    <>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/about">About </Link>
+            </li>
+          </ul>
+        </div>
+
+        <Switch>
+          <Route path="/" exact><Home /></Route>
+          <Route path="/gallery" exact><Gallery /></Route>
+          <Route path="/about" exact><About /></Route>
+        </Switch>
+      </Router>
+
+      <footer>Pequeño footer</footer>
+    </>
+  );
 }
---
+++ mejorashooks/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        <h1>Hola, soy el componente home</h1>
+        <p>Proin in mauris id leo rhoncus luctus quis et risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vitae dapibus metus, ut tincidunt augue. Donec egestas lectus in ante egestas lacinia. Sed eu enim purus. Donec nec tortor ac nulla suscipit cursus. Duis dignissim mattis quam iaculis consectetur. Sed consequat nunc sed fringilla interdum. Ut id mi sagittis, dapibus odio eu, eleifend ex. Pellentesque dictum maximus dignissim. Praesent in lectus ultrices mi viverra venenatis nec at erat. Praesent non vulputate enim. Etiam molestie lobortis pretium.</p>
+        <p>Nam ac neque dolor. Aliquam ullamcorper nec massa sit amet luctus. Vestibulum tincidunt justo id turpis pellentesque, ac molestie lorem euismod. Etiam feugiat scelerisque nisl, id sodales ex cursus ut. Vestibulum imperdiet mauris non ligula bibendum sollicitudin. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque non ipsum dictum, mollis diam ac, auctor mauris. Praesent iaculis iaculis erat, sit amet accumsan nisl fringilla ac. Curabitur ac iaculis purus, eu laoreet felis. Donec ac dolor eget nunc dapibus vulputate vel eget lectus. Mauris ornare, lacus id sollicitudin finibus, odio massa vehicula nibh, in iaculis mi leo sed nisl. Morbi aliquet ipsum orci, laoreet laoreet nisi bibendum sed.</p>
+    </div>
+);
+
+export default Home;
